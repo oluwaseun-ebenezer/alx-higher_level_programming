a5254d89512d9722fee4c6ea2c50e76e06a38cd2
Oluwaseun Aderinlokun <aderinlokunoluwaseun@gmail.com>
2021-09-10 18:51:15 +0100

    fix: Paranthesis issue

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-print(f'{number} {"is positive" if number > 10 else "is negative"} ')
+print(f'{number} {"is positive" if (number > 10) else "is negative"} ')
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f'Last digit of {number} is {str(number)[-1]} {"and is less than 6" if int(str(number)[-1]) < 6 else "and is greater than 5"} {"and is 0" if int(str(number)[-1]) == 0 else "and not 0" }')
+print(f'Last digit of {number} is {str(number)[-1]} {"and is less than 6" if (int(str(number)[-1]) < 6) else "and is greater than 5"} {"and is 0" if (int(str(number)[-1]) == 0) else "and not 0" }')
