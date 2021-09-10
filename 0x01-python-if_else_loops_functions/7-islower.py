dcb51b652439e170f4f95839b5b4b6c723e4dbf5
Oluwaseun Aderinlokun <aderinlokunoluwaseun@gmail.com>
2021-09-10 19:40:42 +0100

    fix

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -7,4 +7,8 @@ def islower(c):
 
 islower = __import__('7-islower').islower
 
-print("c is {}".format("lower" if islower("c") else "upper"))
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
