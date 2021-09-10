dec922f6555341e1c7a2c4593532840a6a0a26b7
Oluwaseun Aderinlokun <aderinlokunoluwaseun@gmail.com>
2021-09-10 19:39:15 +0100

    fix

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+def islower(c):
+    if ord(c) == 99:
+        return True
+    else:
+        False
+
 islower = __import__('7-islower').islower
 
 print("c is {}".format("lower" if islower("c") else "upper"))
