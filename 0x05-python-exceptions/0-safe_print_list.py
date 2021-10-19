4379c4ad0dd6f68c5ea2377ef68d963801d7fb1a
oluwaseun-ebenezer <41710358+oluwaseun-ebenezer@users.noreply.github.com>
2021-10-19 08:53:21 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,9 +7,9 @@ def safe_print_list(my_list=[], _x=0):
     """ safe_print_list """
 
     counter = 0
-    for i in range (_x):
+    for i in range(_x):
         try:
-            print (my_list[i], end="")
+            print(my_list[i], end="")
             counter += 1
         except IndexError:
             break
