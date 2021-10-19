d062452aa23178bce81054563d088f7b9c7d0a68
Oluwaseun Aderinlokun <aderinlokunoluwaseun@gmail.com>
2021-10-19 09:35:16 +0100

    1-safe_print_integer

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" 1-safe_print_integer """
+
+
+def safe_print_list(my_list=[], _x=0):
+    """ safe_print_list_integers """
+
+    counter = 0
+    for i in range(_x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            counter += 1
+        except ValueError or TypeError:
+            continue
+        except IndexError:
+            break
+
+    print()
+
+    return counter
