cb5d01cca172e82d76e5f11371d1953ca6e1e176
oluwaseun-ebenezer <41710358+oluwaseun-ebenezer@users.noreply.github.com>
2021-10-19 08:52:26 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,12 @@
 
 """ 0-safe_print_list """
 
+
 def safe_print_list(my_list=[], _x=0):
     """ safe_print_list """
 
     counter = 0
-    for i in range(_x):
+    for i in range (_x):
         try:
             print (my_list[i], end="")
             counter += 1
