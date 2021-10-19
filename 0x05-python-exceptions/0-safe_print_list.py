595296652359c72845f12887bc4a43a21b73a8e6
Oluwaseun Aderinlokun <aderinlokunoluwaseun@gmail.com>
2021-10-19 08:20:07 +0100

    0. Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+""" 0-safe_print_list """
+
+def safe_print_list(my_list=[], _x=0):
+    """ safe_print_list """
+
+    i = 0
+    for i in range(_x):
+        try:
+            print (my_list[i], end="")
+        except IndexError:
+            break
