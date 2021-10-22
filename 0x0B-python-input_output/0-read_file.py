3fca63a62beb77e725a27358d1913e6c653234f2
Oluwaseun Aderinlokun <aderinlokunoluwaseun@gmail.com>
2021-10-22 10:28:00 +0100

    0x0C. Python - Almost a circle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
-""" 0-safe_print_list """
+"""defining read_file function"""
 
 
 def read_file(filename=""):
-
-    return
+    """reads filename with utf-8"""
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
